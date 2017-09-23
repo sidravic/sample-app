693a47c2b14c0312b8f4c5ec1b2b3cee76df6bd8
siddharth ravichandran <sid.ravichandran@gmail.com>
2017-09-23 12:18:07 +0000

    Dummy server

---
+++ app.js
@@ -0,0 +1,29 @@
+'use strict';
+const uuidv4 = require('uuid/v4');
+const Hapi = require('hapi')
+const Server = new Hapi.Server();
+const serverId = uuidv4()
+
+Server.connection({ port: (process.env.PORT || 3010), host: 'localhost'})
+
+Server.route({
+    method: "GET",
+    path: '/',
+    handler: function(request, reply) {
+        reply({
+            message: "Hello",
+            time: Date.now(),
+            serverId: serverId
+        }).code(200)
+    }
+})
+
+
+Server.start((err) => {
+    if (err){
+        throw err;
+    }
+
+    console.log(`Server running at ${Server.info.uri}`)
+})
+
